b2f2ce7f2ab9f269c4cac05200b2a6f6018bb62a
DianaFarhat <dsf04@mail.aub.edu>
2025-03-08 21:29:23 +0200

    Add colors pallette

---
+++ frontend/src/Utils/colors.js
@@ -0,0 +1,13 @@
+const limeLemonPalette = {
+    neonLime: "#32CD32",
+    goldenLemon: "#FFC300",
+    citrusGlow: "#FFB347",
+    electricLime: "#CCFF00",
+    freshLemon: "#FFF200",
+    limeSplash: "#76FF7A",
+    softCitrus: "#F4E04D",
+    greenZing: "#66CC33",
+};
+  
+export default limeLemonPalette;
+  
