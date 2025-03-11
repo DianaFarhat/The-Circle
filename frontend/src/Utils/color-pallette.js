ac9dedb4bb706842b3c798933b6cb95063b40e3c
DianaFarhat <dsf04@mail.aub.edu>
2025-03-11 15:13:24 +0200

    Add color pallette

---
+++ frontend/src/Utils/color-pallette.js
@@ -0,0 +1,11 @@
+const colorPalette = {
+    limeGreen: "#A6D608",
+    lemonYellow: "#FFD700",
+    citrusZest: "#D4E157",
+    lemonadeCream: "#FFF9C4",
+    zestyOrange: "#FFB300",
+    deepGreenLeaf: "#4CAF50",
+    crispWhite: "#FFFFFF"
+};
+  
+export default colorPalette;
